178f3a2e9783b169ff67ba1237e5dc6bc4abfa79
Sue McClatchy <smcclatchy1@gmail.com>
2025-03-24 14:51:05 -0400

    update data simulation script and data

--- episodes/code/simulated_heart_rates.R
+++ episodes/code/simulated_heart_rates.R
@@ -1,18 +1,29 @@
 # create simulated heart rate data for Generation 100 Study
 
-high <- data.frame("heart_rate" = rnorm(522, mean = 68, sd = 10), 
-                    "exercise_group" = "high intensity", 
-                    "sex" = rep(c("M", "F"), length.out = 522))
+heart_rate <- tibble(sex            = rep(c("M", "F"), c(777, 790)), 
+                     exercise_group = rep(c("control", "moderate intensity", 
+                                            "high intensity"),
+                                          c(780, 387, 400)),
+                     age            = rnorm(1567, mean = 72.8, sd = 1))
 
+heart_rate <- heart_rate %>%
+  mutate(heart_rate = case_when(sex == "M" & exercise_group == "high intensity" ~ 
+                                  rnorm(n = dim(heart_rate)[1], mean = 60, 
+                                        sd = 5),
+                                sex == "M" & exercise_group == "moderate intensity" ~ 
+                                  rnorm(n = dim(heart_rate)[1], mean = 66, 
+                                        sd = 5),
+                                sex == "M" & exercise_group == "control" ~  
+                                  rnorm(n = dim(heart_rate)[1], mean = 70, 
+                                        sd = 5), 
+                                sex == "F" & exercise_group == "high intensity" ~ 
+                                  rnorm(dim(heart_rate)[1], mean = 63, 
+                                        sd = 5), 
+                                sex == "F" & exercise_group == "moderate intensity" ~ 
+                                  rnorm(dim(heart_rate)[1], mean = 69, 
+                                        sd = 5), 
+                                sex == "F" & exercise_group == "control" ~ 
+                                  rnorm(dim(heart_rate)[1], mean = 73, 
+                                        sd = 5)))
 
-moderate <- data.frame("heart_rate" = rnorm(522, mean = 70, sd = 10), 
-                       "exercise_group" = "moderate intensity", 
-                       "sex" = rep(c("M", "F"), length.out = 522))
-
-control <- data.frame("heart_rate" = rnorm(522, mean = 72, sd = 10), 
-                      "exercise_group" = "control", 
-                      "sex" = rep(c("M", "F"), length.out = 522))
-
-simulated_heart_rates <- rbind(high, moderate, control)
-
-write_csv(simulated_heart_rates, file = "data/simulated_heart_rates.csv")
+write_csv(heart_rate, file = "data/simulated_heart_rates.csv")
